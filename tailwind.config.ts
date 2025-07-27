50a46d2131ba1adfba2d58c381584b9c00fcb8a0
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-07-27 18:14:14 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,32 @@ const config: Config = {
       fontFamily: {
         'press-start': ['"Press Start 2P"', 'cursive'],
       },
+      colors: {
+        sky: '#5c94fc',
+        ground: '#e69039',
+        brick: '#c27334',
+        pipe: '#00a800',
+        pipe_highlight: '#64dd17',
+      },
+      keyframes: {
+        bob: {
+          '0%, 100%': { transform: 'translateY(-4px)' },
+          '50%': { transform: 'translateY(0)' },
+        },
+        pulse: {
+          '0%, 100%': { transform: 'scale(1)', 'box-shadow': '0 0 10px #fce462, 0 0 20px #fce462' },
+          '50%': { transform: 'scale(1.05)', 'box-shadow': '0 0 20px #fce462, 0 0 40px #fce462' },
+        },
+        characterIdle: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-8px)' },
+        }
+      },
+      animation: {
+        bob: 'bob 2s ease-in-out infinite',
+        pulse: 'pulse 1.5s ease-in-out infinite',
+        characterIdle: 'characterIdle 1s ease-in-out infinite'
+      }
     },
   },
   plugins: [],
