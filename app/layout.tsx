047fd3b233b376e9d2c7ef09a148f7b6a46fc5db
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-07-27 18:15:14 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "My Retro Website",
-  description: "Website pribadi dengan gaya game retro.",
+  title: "CSS-Only Retro Site",
+  description: "A responsive retro game style website built only with CSS.",
 };
 
 export default function RootLayout({
@@ -13,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
